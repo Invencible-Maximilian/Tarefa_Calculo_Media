f26cab358f225f34ac318c2ce81fc492e5e22378
Invencible-Maximilian <maximilianwachter@outlook.com>
2025-09-04 22:01:42 -0300

    Primeiro commit

---
+++ MediaValores.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MediaValores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        System.out.print("Digite a quarta nota: ");
+        double nota4 = scanner.nextDouble();
+
+        System.out.printf("Resultado da média: %.2f",(nota1+nota2+nota3+nota4)/4 );
+    }
+}
